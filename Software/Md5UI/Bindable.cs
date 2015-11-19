cbd79378a503221f26592960871ff44854bf3bbb
John Leitch <JohnLeitch@outlook.com>
2015-11-19 03:27:25 +0000

    Fixed reference error.

---
+++ Software/Md5UI/Bindable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components
+{
+    public abstract class Bindable : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void InvokePropertyChanged([CallerMemberName] string callerName = null)
+        {
+            var p = PropertyChanged;
+
+            if (p != null)
+            {
+                p(this, new PropertyChangedEventArgs(callerName));
+            }
+        }
+
+        protected void SetProperty<T>(ref T property, T value, [CallerMemberName] string callerName = null)
+        {
+            property = value;
+            InvokePropertyChanged(callerName);
+        }
+    }
+}
